afc03c0304ef161182707c9b046cbe179c1e7f62
Connor Savoy <connorsavoy812@gmail.com>
2023-02-28 13:46:07 -0500

    added javascript and json data to populate the database once its created

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,14 @@
 const sequelize = require('../config/connection');
+const Recipe = require('../models');
+const recipes = require('./recipe.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+
+  await Recipe.bulkCreate(recipes, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
