3f52d0349cf8f7e951875139b2ac002c587cbb5d
Connor Savoy <connorsavoy812@gmail.com>
2023-03-02 13:41:37 -0500

    added a favourites routes in api to add a favourite recipe to user

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const recipeRoutes = require('./recipe-routes');
+const favouriteRoutes = require('./favourites');
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
+router.use('/favourites', favouriteRoutes);
 
 module.exports = router;
