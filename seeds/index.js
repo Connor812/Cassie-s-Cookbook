69f72950f450c3e972a2cb9b6dc47155c3a8f078
Connor Savoy <connorsavoy812@gmail.com>
2023-03-02 09:43:22 -0500

    added the favourite feature

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { Recipe, User, Ingredient, RecipeIngredients, Review, Favourites } = require('../models');
+const { Recipe, User, Ingredient, RecipeIngredients, Review } = require('../models');
 
 const recipes = require('./recipe.json');
 const users = require('./user.json');
@@ -8,8 +8,6 @@ const recipeIngredients = require('./recipe-ingredients.json');
 const favourites = require('./favourites.json');
 const Favourites = require('../models/favourites');
 const reviews = require('./reviews.json');
-
-
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
