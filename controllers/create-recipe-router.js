3d9557b3f572d9f8a81bf557cd9c7870ed256bc0
elkirko <kirkhagglund@gmail.com>
2023-03-03 17:25:32 -0600

    Added dashboard layout and styling

--- controllers/create-recipe-router.js
+++ controllers/create-recipe-router.js
@@ -1,17 +1,10 @@
 const router = require('express').Router();
 
-
 router.get('/', (req, res) => {
     res.render('create-recipe', {
         loggedIn: req.session.loggedIn,
         username: req.session.username
     });
 });
-
-// router.post('/', (req, res) => {
-
-// });
-
-
 
 module.exports = router;
