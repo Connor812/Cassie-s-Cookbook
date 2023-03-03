ce6236b80aca39d0a39e9c301c3a9c9fec63fc8e
elkirko <kirkhagglund@gmail.com>
2023-03-03 17:25:32 -0600

    Added dashboard layout and styling

--- public/js/anime.js
+++ public/js/anime.js
@@ -2,10 +2,11 @@
     targets: '.letter',
     opacity: 0,
     translateX: 250,
+    translateY: -50,
     rotate: {
         value: 360,
     },
     direction: 'reverse',
     easing: 'easeInOutSine',
-    delay: anime.stagger(50, {start: 250})
+    delay: anime.stagger(50, {start: 200})
   });
